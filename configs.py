6f68817c97d178d97b889512533f90be84129608
Rafael Lopes <dev@rafalop.es>
2013-12-09 21:46:55 -0200

    Adding a sample configuration file.

---
+++ configs.py
@@ -0,0 +1,12 @@
+zabbixapiurl = 'your zabbix url'
+zabbixapilogin = 'your zabbix login'
+zabbixapipasswd = 'your zabbix passwd'
+ecopowerstart = '20:00:00'
+ecopowerfinish = '09:00:00'
+
+bridgepasswd = 'PyZabbixHuePasswd'
+
+red = [0.800,0.300]
+orange = [0.700,0.400]
+yellow = [0.700, 0.500]
+white = [0.350,0.350]
