322a6a06d7e203277e2c3cc7869087fabd808641
root <root@raspberrypi.(none)>
2014-09-22 21:57:31 +0000

    Adding exception handling and importing only things I'll need

--- __main__.py
+++ __main__.py
@@ -1,19 +1,22 @@
 from zabbixdata import *
 from phue import *
-import time
+from time import sleep
+from configs import runinterval
 
 phue = PHue()
 
 while True:
+    try:
+        zdata = ZabbixData()
+        zdata.getunacktriggers()
+        zdata.getworsttrigger()
+        print zdata.worsttrigger
 
-    zdata = ZabbixData()
-    zdata.getunacktriggers()
-    zdata.getworsttrigger()
-    print zdata.worsttrigger
-
-    phue.bootstrap()
-    phue.imalive(zdata.worsttrigger)
-    phue.updategroup(0,zdata.worsttrigger)
-    phue.pushgroup()
-
-    sleep(60)
+        phue.bootstrap()
+        phue.imalive(zdata.worsttrigger)
+        phue.updategroup(0,zdata.worsttrigger)
+        phue.pushgroup()
+    except Exception, e:
+        print str(e)
+    finally:
+        sleep(runinterval)
